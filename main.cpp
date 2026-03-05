ae79d6977b2d8ff828f48f3b474e4df277683491
jangu5318008 <jasonnguyen185@gmail.com>
2026-03-04 18:53:21 -0800

    1st Skeleton

--- main.cpp
+++ main.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
+#include "TextGraphics.h"
+
+using namespace std;
 
 const int N = 10;
 void initBoard(char board[N][N], int n);
 void printBoard(char board[N][N], int n);
 bool isGameOver(char board[N][N], int n);
 void getPlayerInput(char board[N][N], int n, char playerChar);
+
 int main() {
 
+    TextGraphics graphics;  
+
+    char board[N][N];
+    cout << "How big of a board do you want?";
+    int n; 
+    cin >> n;
+
+    initBoard(board, n); 
+    printBoard(board, n);
+    while (!isGameOver(board,n)) {
+        //
+    }
+void initBoard(char board [N][N], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << board[i][j] << ' ';
+        }
+    cout << '\n';
+}
+}
+bool isGameOver(char board[N][N], int n) {
 
 
+
+    return false;
+}
 
 
 
